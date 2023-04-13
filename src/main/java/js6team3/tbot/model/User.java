cf6c995434c1dcd81b2bb7f588b14c405f9fb230
sillyEjevika <Doroteia_Vika@mail.ru>
2023-04-13 17:38:24 +0300

    Добавлен функционал в User

--- src/main/java/js6team3/tbot/model/User.java
+++ src/main/java/js6team3/tbot/model/User.java
@@ -1,5 +1,4 @@
 package js6team3.tbot.model;
-import lombok.*;
 import javax.persistence.*;
 
 
@@ -8,12 +7,7 @@ import javax.persistence.*;
  * @author Loginova Viktoria
  * @version 1.0.0
  */
-@Getter
-@Setter
-@EqualsAndHashCode
-@ToString
-@AllArgsConstructor
-@NoArgsConstructor
+
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -39,4 +33,60 @@ public class User {
     @ManyToOne
     @JoinColumn(name = "cat_id")
     private Cat cat;
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getUserPhoneNumber() {
+        return userPhoneNumber;
+    }
+
+    public void setUserPhoneNumber(String userPhoneNumber) {
+        this.userPhoneNumber = userPhoneNumber;
+    }
+
+    public String getUserEmail() {
+        return userEmail;
+    }
+
+    public void setUserEmail(String userEmail) {
+        this.userEmail = userEmail;
+    }
+
+    public Dog getDog() {
+        return dog;
+    }
+
+    public void setDog(Dog dog) {
+        this.dog = dog;
+    }
+
+    public Cat getCat() {
+        return cat;
+    }
+
+    public void setCat(Cat cat) {
+        this.cat = cat;
+    }
 }
+
