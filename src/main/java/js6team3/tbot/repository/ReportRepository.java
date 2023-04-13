8911978c161f99e921f6451db652163827a6dcba
Andrei <119403547+zalex14@users.noreply.github.com>
2023-04-13 17:01:46 +0300

    Update ReportRepository.java

--- src/main/java/js6team3/tbot/repository/ReportRepository.java
+++ src/main/java/js6team3/tbot/repository/ReportRepository.java
@@ -1,4 +1,13 @@
 package js6team3.tbot.repository;
 
-public class ReportRepository {
+import js6team3.tbot.entity.report.Report;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+/**
+ *
+ */
+public interface ReportRepository extends JpaRepository<Report,Long> {
+    List<Report> getAllBy();
 }
