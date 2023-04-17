cf3dd08ff19217e787b7d8e9cc15952ccce9bc18
zalex14 <9824018@gmail.com>
2023-04-17 16:02:22 +0300

    JS6_Team3 Report / ReportPhoto & sql & add liquidbase

--- src/main/java/js6team3/tbot/TBotApplication.java
+++ src/main/java/js6team3/tbot/TBotApplication.java
@@ -2,10 +2,9 @@ package js6team3.tbot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
-@EnableScheduling
+//@EnableScheduling
 public class TBotApplication {
 
     public static void main(String[] args) {
